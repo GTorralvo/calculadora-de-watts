fe29f35c735349acbacccc6afbd883ba9dfc4d88
GTorralvo <gamestorralvo002@gmail.com>
2026-03-20 08:34:08 -0300

    Projeto Finalizado

--- app.js
+++ app.js
@@ -6,13 +6,10 @@ let valormensagem = 0
 let button = document.getElementById("button")
 let resultado = document.getElementById("resultado")
 
-
 function calcularTotal(){
-
 
     let consumomedio = Number(consumo.value)
     let total
-
 
     if(consumomedio <= 100){
         total = consumomedio * valor1
@@ -27,12 +24,10 @@ function calcularTotal(){
         valormensagem = valor3
     }
 
-
     resultado.innerHTML = `
-    <p>O valor total da conta é R$ ${total.toFixed(2)}</p>
-    <p> Voce vai pagar R$${valormensagem.toFixed(2)} por kwh</p>
+        <p>O valor total da conta é R$ ${total.toFixed(2)}</p>
+        <p>Você vai pagar R$${valormensagem.toFixed(2)} por kWh</p>
     `
 }
-
 
 button.addEventListener("click", calcularTotal)
